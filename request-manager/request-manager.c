caa1e8c9c60664db85dbcaabe98d60287560849e
Dan Borah <danielborah@gmail.com>
2023-06-18 15:49:41 -0500

    more prep for poll(2)

--- request-manager/request-manager.c
+++ request-manager/request-manager.c
@@ -3,61 +3,35 @@
 int
 main(int argc, char *argv[]) {
 
+    char *port = argv[1];
+
+    if (is_port_invalid(port)) {
+        perror("port is invalid");
+    }
+
     setup_port(argv[1]);
 
-    listen_for_requests();
+    listen_for_requests(argv[1]);
     
 
-    // if (bind(listen_fd, res->ai_addr, res->ai_addrlen) < 0) {
-        // perror("bind");
-    // }
-// 
-    // /* start listening */
-    // if (listen(listen_fd, BACKLOG) < 0) {
-        // perror("listen");
-    // }
-// 
-    // printf("Threaded HTTP Server waiting on port %s\n", listen_port); 
-// 
-    // addrlen = sizeof(remote_sa);
-// 
-    // /* Set up poll structure */
-// 
-    // nfds_t nfds = 0;
-    // struct pollfd *poll_fds;
-    // int  num_fds = 0;
-// 
-    // if ((poll_fds = malloc(MAX_FDS * sizeof(struct pollfd))) == NULL) {
-        // perror("malloc");
-    // }
-// 
-    // poll_fds->fd = listen_fd;
-    // poll_fds->events = POLLIN;
-    // poll_fds->revents = 0;
-    // num_fds += 1;
 }
 
-
-void
+int
 setup_port(char *port) {
+    /**
+     * DESCRIPTION: Prepares for listening to a port. Creates socket and binds to a port
+     * PARAMS:
+     *  port: String of digits that represents port to bind to
+     * RETURNS: The file descriptor to listen on for poll
+    */
 
-    char *listen_port;
-    int listen_fd, new_fd;
+    int listen_fd, rc;
     struct addrinfo hints, *res;
-    int rc;
-    struct sockaddr_in remote_sa;
-    uint16_t remote_port;
-    socklen_t addrlen;
-    char *remote_ip;
-
-    int bytes_received;
-    char input_buf[BUFF_SIZE];
-
-    listen_port = port;
 
     /* create a socket */
     if ((listen_fd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket");
+        exit(1);
     }
 
     /* bind it to a port */
@@ -66,13 +40,84 @@ setup_port(char *port) {
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    if((rc = getaddrinfo(NULL, listen_port, &hints, &res)) != 0) {
-        printf("getaddrinfo failed: %s\n", gai_strerror(rc));
+    if ((rc = getaddrinfo(NULL, port, &hints, &res)) != 0) {
+        perror("getaddrinfo failed: %s\n", gai_strerror(rc));
+        freeaddrinfo(res);
         exit(1);
     }
+
+    if (bind(listen_fd, res->ai_addr, res->ai_addrlen) < 0) {
+        perror("bind");
+        freeaddrinfo(res);
+        exit(1);
+    }
+
+    freeaddrinfo(res);
+
+    return listen_fd;
 }
 
 void
-listen_for_requests() {
+listen_for_requests(char *port) {
+    socklen_t addrlen;
+    struct sockaddr_in remote_sa;
 
+    /* start listening */
+    if (listen(port, BACKLOG) < 0) {
+        perror("listen");
+        exit(1);
+    }
+
+    printf("Threaded HTTP Server waiting on port %s\n", port); 
+
+    addrlen = sizeof(remote_sa);
+
+    /* Set up poll structure */
+
+    nfds_t nfds = 0;
+    struct pollfd *poll_fds;
+
+    if (setup_poll_struct(poll_fds, port)) {
+        perror("malloc");
+        free(poll_fds);
+        exit(1);
+    }
+
+    int num_fds = 1;
+    free(poll_fds);
+}
+
+int
+setup_poll_struct(struct pollfd *poll_fds, char *port) {
+
+    if ((poll_fds = malloc(MAX_FDS * sizeof(struct pollfd))) == NULL) {
+        return 1;
+    }
+
+    poll_fds->fd = port;
+    poll_fds->events = POLLIN;
+    poll_fds->revents = 0;
+
+    return 0;
+}
+
+int
+is_port_invalid(char *port) {
+    if (isnumber(port)) {
+        return 1;
+    }
+    return 0;
+}
+
+int
+isnumber(char *s) {
+    int length = strlen(s);
+
+    for (int i = 0; i < length; i++) {
+        if (!isdigit(s[i])) {
+            return 0;
+        }
+    }
+
+    return 1;
 }
--- request-manager/request-manager.h
+++ request-manager/request-manager.h
@@ -3,7 +3,13 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <ctype.h>
+#include <poll.h>
 
 #define BUFF_SIZE 4000
 #define MAX_FDS 20
 #define BACKLOG 20
+
+int is_port_invalid(char *port);
+int setup_port(char *port);
+int setup_poll_struct(struct pollfd *poll_fds, char *port)
